098dd3c56aa8872c95fecf2d380084ce272980c2
poezedoez <raggerjonkers@gmail.com>
2020-06-29 11:04:44 +0200

    update error

--- eval_all.py
+++ eval_all.py
@@ -6,5 +6,5 @@ for framework in ["speer", "spert"]:
             for run in [1, 2, 3]:
                 print("Evaluating {} {} on {} (run {})".format(framework, model, dataset, run)) 
                 subprocess.run(["python", "main.py", "eval", 
-                                "--config", "configs/{}/{}/{}_eval.conf",
+                                "--config", "configs/{}/{}/{}_eval.conf".format(framework, model, dataset),
                                 "--model_path", "data/{}/save/{}_eval_{}/run{}/".format(framework, dataset, model, run)])
