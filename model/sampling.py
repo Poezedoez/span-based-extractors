e522448ae9e80b27f3845eb03aca418a86f8fc60
Poezedoez <raggerjonkers@gmail.com>
2020-08-13 15:30:29 +0200

    syntax error

--- model/sampling.py
+++ model/sampling.py
@@ -456,7 +456,7 @@ def _create_eval_sample(doc, max_span_size, context_size, type_key="type_index",
     # entities
     entity_masks = torch.stack(entity_masks) if entity_masks else torch.zeros(1, context_size)
     entity_sizes = torch.tensor(entity_sizes, dtype=torch.long) if entity_sizes else torch.tensor([0], dtype=torch.long)
-    entity_spans = torch.tensor(entity_spans, dtype=torch.long) if  entity_spans torch.tensor([(0,0)], dtype=torch.long)
+    entity_spans = torch.tensor(entity_spans, dtype=torch.long) if  entity_spans else torch.tensor([(0,0)], dtype=torch.long)
 
     return EvalTensorSample(encoding=encoding, ctx_mask=ctx_mask, entity_masks=entity_masks,
                             entity_sizes=entity_sizes, entity_spans=entity_spans, 
